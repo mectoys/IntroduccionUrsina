74e4f0d7da2d3d879caebddf495cf7787240aad2
mectoys <mectoys2013@gmail.com>
2023-04-17 21:36:13 -0500

    Crea Videojuegos con Ursina Engine powered by Python.

--- 8_Ejercicio_Modulo_A.py
+++ 8_Ejercicio_Modulo_A.py
@@ -3,46 +3,46 @@ from ursina import *
 '''Mini reproductor
 Boton play/pause, stop, control de volumen(+,-)'''
 app = Ursina()
-audio =None
+audio = None
 window.fps_counter.visible = False
-audio_paused =False
+audio_paused = False
+
 
 def PlayButton():
     global audio, audio_paused
     if audio is not None and audio.playing:
         audio.pause()
-        audio_paused=True
+        audio_paused = True
     elif audio is not None and audio_paused:
         audio.resume()
-        audio_paused=False
+        audio_paused = False
     else:
-        audio= Audio()
-        audio.clip='audio//TMNT1.mp3'
+        audio = Audio()
+        audio.clip = 'audio//TMNT1.mp3'
         audio.play()
-        print_on_screen(round(audio.volume,2), position=(0,0.2))
-
+        print_on_screen(round(audio.volume, 2), position=(0, 0.2))
 
 
 def StopButton():
     global audio, audio_paused
     if audio is not None:
         audio.stop()
-        audio=None
-        audio_paused=False
+        audio = None
+        audio_paused = False
 
 
 def Increase_volumen():
     global audio, audio_paused
     if audio is not None:
-        audio.volume+=0.1
-        print_on_screen(round(audio.volume,2), position=(0, 0.2))
+        audio.volume += 0.1
+        print_on_screen(round(audio.volume, 2), position=(0, 0.2))
 
 
 def Decrease_volumen():
     global audio, audio_paused
     if audio is not None:
-        audio.volume-=0.1
-        print_on_screen(round(audio.volume,2), position=(0, 0.2))
+        audio.volume -= 0.1
+        print_on_screen(round(audio.volume, 2), position=(0, 0.2))
 
 
 # Botones
